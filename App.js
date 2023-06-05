be5aede143f8ea84259167ac1e4d9927800ef083
madhumithasr <madhukaan@gmail.com>
2023-06-05 17:51:32 +0530

    Second initial commit

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unstable-nested-components */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -17,19 +18,49 @@ import {SafeAreaProvider} from 'react-native-safe-area-context';
 import Home from './src/screen/app/Home';
 import Favorites from './src/screen/app/Favorites';
 import Profile from './src/screen/app/Profile';
+import {Image} from 'react-native';
+import Setting from './src/screen/app/Setting';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
-const Tabs = () => {
-  <Tab.Navigator>
+const Tabs = () => (
+  <Tab.Navigator
+    screenOptions={({route}) => ({
+      tabBarIcon: ({focused, color, size}) => {
+        let icon;
+
+        if (route.name === 'Home') {
+          icon = focused
+            ? require('./src/assets/tabs/home_active.png')
+            : require('./src/assets/tabs/home.png');
+        } else if (route.name === 'Profile') {
+          icon = focused
+            ? require('./src/assets/tabs/profile_active.png')
+            : require('./src/assets/tabs/profile.png');
+        } else if (route.name === 'Favorites') {
+          icon = focused
+            ? require('./src/assets/tabs/bookmark_active.png')
+            : require('./src/assets/tabs/bookmark.png');
+        }
+
+        // You can return any component that you like here!
+        // eslint-disable-next-line react-native/no-inline-styles
+        return <Image style={{width: 24, height: 24}} source={icon} />;
+      },
+      headerShown: false,
+      tabBarShowLabel: false,
+      tabBarStyle: {borderTopColor: colors.lightGrey},
+    })}>
     <Tab.Screen name="Home" component={Home} />
     <Tab.Screen name="Favorites" component={Favorites} />
     <Tab.Screen name="Profile" component={Profile} />
-  </Tab.Navigator>;
-};
+  </Tab.Navigator>
+);
+
 const App = () => {
   const isSignedIn = true;
+
   const theme = {
     colors: {
       background: colors.white,

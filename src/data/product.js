b32e7b36e7c6d40e86b54ed0b03334e6ec335dda
madhumithasr <madhukaan@gmail.com>
2023-06-06 13:38:21 +0530

    second description

--- src/data/product.js
+++ src/data/product.js
@@ -7,6 +7,8 @@ export const product = [
       'https://grandrapidschair.com/wp-content/uploads/2016/01/250_Brady_Graphite_Honey-1.jpg',
     category: 1,
     price: '$ 20.00',
+    description:
+      'Minimal Stand is made of by natural wood. The design that is very simple and minimal. This is truly one of the best furnitures in any family for now. With 3 different colors, you can easily select the best match for your home. ',
   },
   {
     id: 2,
@@ -15,6 +17,8 @@ export const product = [
       'https://ii1.pepperfry.com/media/catalog/product/f/o/800x880/foldable-table-in-natural-colour-by-clasicraft-foldable-table-in-natural-colour-by-clasicraft-ahmtub.jpg',
     category: 2,
     price: '$ 25.00',
+    description:
+      'Minimal Stand is made of by natural wood. The design that is very simple and minimal. This is truly one of the best furnitures in any family for now. With 3 different colors, you can easily select the best match for your home. ',
   },
   {
     id: 3,
@@ -23,6 +27,8 @@ export const product = [
       'https://www.woods-furniture.co.uk/images/products/standard/7706_8118.jpg',
     category: 3,
     price: '$ 45.00',
+    description:
+      'Minimal Stand is made of by natural wood. The design that is very simple and minimal. This is truly one of the best furnitures in any family for now. With 3 different colors, you can easily select the best match for your home. ',
   },
   {
     id: 4,
@@ -31,6 +37,8 @@ export const product = [
       'https://cdn.shopify.com/s/files/1/0079/2539/9616/products/myakka-chairs-jalkamal-block-print-footstool-28730051264703_2000x.jpg?v=1629217511',
     category: 3,
     price: '$ 80.00',
+    description:
+      'Minimal Stand is made of by natural wood. The design that is very simple and minimal. This is truly one of the best furnitures in any family for now. With 3 different colors, you can easily select the best match for your home. ',
   },
   {
     id: 5,
@@ -44,5 +52,7 @@ export const product = [
     ],
     category: 4,
     price: '$ 120.00',
+    description:
+      'Minimal Stand is made of by natural wood. The design that is very simple and minimal. This is truly one of the best furnitures in any family for now. With 3 different colors, you can easily select the best match for your home. ',
   },
 ];
